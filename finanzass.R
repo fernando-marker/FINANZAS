68a2300078e906ea7ea8cf6ec6b54b291fb60016
fernando2003 <ferdrake.20@gmail.com>
2023-09-10 22:13:30 -0500

    finanzas termiando

---
+++ finanzass.R
@@ -0,0 +1,15 @@
+myfunction= function(x=rnorm(100),mean (x))
+  myfunction 
+x=rnorm(100)#variable normales
+x
+mean=x
+mean
+function!?
+  is()  
+mean(2;4;5;6)
+mean
+source()
+msj<-"hello"
+msj
+x<-(1:20)
+x
